c1b575d7830dd4439eda249ab36bf5d3ad8e8555
yangyue <m18710434142_1@163.com>
2016-07-18 21:15:55 +0800

    sad

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,3 +1,62 @@
 'use strict';
+function printReceipt(inputs){
+  let items=buildItems(inputs);
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+  return items;
+}
+
+function buildItems(inputs){
+  let cartItems=[];
+  const exinputs=changeInputs(inputs);
+  const allItems=loadAllItems();
+  let i;
+
+  for(i=0;i<exinputs.length;i++){
+    allItems.forEach(function(item){
+      if(exinputs[i]===item.barcode){
+        let cartItem=isexit(cartItems,exinputs[i]);
+
+        if(cartItem){
+          cartItem.count++;
+        }
+          else{
+          cartItems.push({item:item,count:1});
+          }
+        }
+    });
+  }
+
+  return cartItems;
+}
+
+function isexit(cartItems,barcode){
+  let flag;
+
+  cartItems.forEach(function (cartItem){
+    if(cartItem.item.barcode===barcode){
+      flag=cartItem;
+    }
+  });
+
+  return flag;
+}
+
+function changeInputs(inputs){
+  let i;
+  let exinputs=[];
+
+  for(i=0;i<inputs.length;i++){
+    if(inputs[i].match('-')){
+      let num=inputs[i].substring(11);
+      while(num>0){
+        exinputs.push(inputs[i].substring(0,10));
+        num--;
+      }
+    }
+    else{
+      exinputs.push(inputs[i]);
+    }
+  }
+
+  return exinputs;
+}
--- pos_v1/test/main-test.js
+++ pos_v1/test/main-test.js
@@ -34,4 +34,39 @@ describe('pos', () => {
 
     expect(console.log).toHaveBeenCalledWith(expectText);
   });
+  it('print items', () => {
+
+    var countItems = buildItems(inputs);
+
+    const correctItems = [
+      {
+        item: {
+          barcode: 'ITEM000001',
+          name: '雪碧',
+          unit: '瓶',
+          price: 3.00
+        },
+        count: 5
+      },
+      {
+        item: {
+          barcode: 'ITEM000003',
+          name: '荔枝',
+          unit: '斤',
+          price: 15.00
+        },
+        count: 2
+      },
+      {
+        item: {
+          barcode: 'ITEM000005',
+          name: '方便面',
+          unit: '袋',
+          price: 4.50
+        },
+        count: 3
+      }
+    ];
+    expect(countItems).toEqual(correctItems);
+  });
 });
